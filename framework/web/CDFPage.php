bdae00544636839017fa05b75b931d578a1eb463
SND\cleardemon_cp <SND\cleardemon_cp@527b9aa4-23f3-4c7d-acba-bb8337ca51bc>
2011-12-21 12:39:10 +0000

    removed Savant3 support

--- framework/web/CDFPage.php
+++ framework/web/CDFPage.php
@@ -1,143 +1,14 @@
 <?php
 	/**
-	 * Renders an HTML document to the requesting client (browser).
-	 * <code>
-	 *	$page = new CDFPage();
-	 *	$page->setPageTitle('Moo');
-	 *	$page->render('main/home');
-	 * </code>
-	 *
-	 * @author demon
+	 * Various utility for HTTP when working with browser-requested resources.
+	 * This used to be a wrapper for Savant but template functionality is done better with other libraries.
 	 */
 
-	require_once dirname(__FILE__) . '/../import/savant/Savant3.php';
 	require_once dirname(__FILE__) . '/../core/CDFExceptions.php';
 	require_once dirname(__FILE__) . '/../core/CDFDataHelper.php';
 
 	class CDFPage
 	{
-		/**
-		 * @var Savant3 Template renderer
-		 */
-		private $_template;
-		/**
-		 * @var string Filename of template to render
-		 */
-		private $_templatefile;
-		private $_coretemplatefile;
-		private $_attributes;
-
-		/**
-		 * @param string $templatepath Disk location of where templates are stored.
-		 * @param string $coretemplate Template file that represents an entire HTML page.
-		 */
-		public function __construct($templatepath, $coretemplate = null)
-		{
-			$this->_attributes = array();
-			$this->_template = new Savant3(array(
-				'template_path' => $templatepath,
-				'exceptions' => true
-			));
-			$this->_template->assignRef('Page', $this);
-			$this->_coretemplatefile = $coretemplate;
-		}
-
-		//
-		// Attributes
-		// These are general purpose settings that can be queried by the template render,
-		// but are local to this instance of a Page.
-		//
-
-		/**
-		 * Sets an attribute for this page.
-		 * @param string $key Key to index attribute.
-		 * @param mixed $value Value of the attribute.
-		 */
-		protected function setAttribute($key, $value)
-		{
-			if(is_string($key) == false)
-				throw new Exception('Invalid key for Page attribute');
-			$this->_attributes[$key] = $value;
-		}
-
-		/**
-		 * Retrieves a named attribute defined for this page.
-		 * @param string $key Attribute index key.
-		 * @return mixed The value of the attribute, or a string containing the name if not found.
-		 */
-		public function getAttribute($key)
-		{
-			if(array_key_exists($key, $this->_attributes) == false)
-				return CDFDataHelper::AsStringSafe($key);
-
-			return $this->_attributes[$key];
-		}
-
-		/**
-		 * Defines the page title for the HTML document.  This would be queried by the template.
-		 * @param string $title Title to use.
-		 */
-		public function setPageTitle($title)
-		{
-			if(is_string($title) == false)
-				throw new CDFInvalidArgumentException('Page title must be a string');
-
-			$this->setAttribute('PageTitle', CDFDataHelper::AsStringSafe($title));
-		}
-
-		//
-		// Rendering
-		//
-
-		/**
-		 * Renders the complete HTML document to the client.<br/>
-		 * @param string $templatename Name of the template that defines the content for the HTML document (no extensions).
-		 */
-		public function render($templatename)
-		{
-			if($this->_coretemplatefile !== null)
-			{
-				$this->_templatefile = $templatename . '.tpl.php';
-				ob_start();
-				$this->_template->display($this->_coretemplatefile);
-				ob_end_flush();
-			}
-			else
-			{
-				// no core template, treat $templatename as a whole page. calls to renderContent() will fail.
-				ob_start();
-				$this->_template->display($templatename . '.tpl.php');
-				ob_end_flush();
-			}
-		}
-
-		/**
-		 * [INTERNAL] Allows the container template (core template) to render its content.
-		 * This function should not be called unless it is in a core template (e.g. menu.tpl.php).
-		 */
-		public function renderContent()
-		{
-			if($this->_coretemplatefile === null)
-				throw new CDFInvalidOperationException('renderContent cannot be called if page is standalone');
-			$this->_template->display($this->_templatefile);
-		}
-
-		/**
-		 * Defines a value for use in the page template.
-		 * @param string $varkey Key that the page template will recognise.
-		 * @param mixed $varvalue Any object, string, etc. for the template to use.
-		 */
-		public function setTemplateVariable($varkey, $varvalue)
-		{
-			if(!is_string($varkey))
-				throw new CDFInvalidArgumentException('Template key must be string');
-			$this->_template->assign($varkey, $varvalue);
-		}
-
-		//
-		// HTTP utility
-		//
-
 		/**
 		 * Tests if the current request is over HTTPS.
 		 * @return bool True if the remote request is via HTTPS
@@ -170,13 +41,13 @@
 
 		/**
 		 * Returns the current URI/URL for the request, optionally stripping the query string.
-		 * @param bool $keepquery Set to true to preserve the query string, false to remove it.
+		 * @param bool $keepQuery Set to true to preserve the query string, false to remove it.
 		 * @return string
 		 */
-		public static function getCurrentUri($keepquery = true)
+		public static function getCurrentUri($keepQuery = true)
 		{
 			$url = $_SERVER['REQUEST_URI'];
-			if($keepquery == false)
+			if($keepQuery == false)
 			{
 				$pos = strpos($url, '?');
 				if($pos !== false)
@@ -194,11 +65,11 @@
 		/**
 		 * Causes the client to redirect to the same page.
 		 * Script execution will end if calling this.
-		 * @param bool $keepquery Set to true to preserve the query string, false to remove it.
+		 * @param bool $keepQuery Set to true to preserve the query string, false to remove it.
 		 */
-		public function reload($keepquery = true)
+		public function reload($keepQuery = true)
 		{
-			$this->redirect($this->getCurrentUri($keepquery));
+			$this->redirect($this->getCurrentUri($keepQuery));
 		}
 
 		//
@@ -207,17 +78,17 @@
 
 		/**
 		 * Tests if the current request has HTTP POST data, optionally originating from a named "submit" button.
-		 * @param string|null $submitname Name of the <input type="submit"> form field or null if don't care.
+		 * @param string|null $submitName Name of the <input type="submit"> form field or null if don't care.
 		 * @return bool True if post back detected.
 		 */
-		public function isPostBack($submitname = null)
+		public function isPostBack($submitName = null)
 		{
 			// if no submit button name passed in, just check if there was any post variables
-			if($submitname == null)
+			if($submitName == null)
 				return count($_POST) > 0;
 
 			// check if the specified form field is present in the postback
-			return isset($_POST[$submitname]);
+			return isset($_POST[$submitName]);
 		}
 
 		/**
